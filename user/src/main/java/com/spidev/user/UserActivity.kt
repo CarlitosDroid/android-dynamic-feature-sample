86f166443c1cafc61a47de234b57390fb5c869fc
Carlos Leonardo Camilo <carlosleonardovargashuaman@gmail.com>
2019-03-21 13:04:55 -0500

    Update

---
+++ user/src/main/java/com/spidev/user/UserActivity.kt
@@ -0,0 +1,22 @@
+package com.spidev.user
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_user.*
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
